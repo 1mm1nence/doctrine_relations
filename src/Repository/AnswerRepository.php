42e1e76c064ee86d8478e93323be21b891d5e157
1mm1nence <110335004+1mm1nence@users.noreply.github.com>
2023-11-22 15:29:12 +0200

    moved criteria to repo as a static method

--- src/Repository/AnswerRepository.php
+++ src/Repository/AnswerRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Answer;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +18,12 @@ class AnswerRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Answer::class);
+    }
+
+    public static function createApprovedCriteria(): Criteria
+    {
+        return Criteria::create()
+            ->andWhere(Criteria::expr()->eq('status', Answer::STATUS_APPROVED));
     }
 
     // /**
